c06e70cc4e227564d06c5f96b6758fe52774217b
Oleksii Kravchenko <djdesidos@gmail.com>
2022-10-17 21:58:42 +0300

    first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,89 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Notify } from 'notiflix';
+// conponents //
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Section from './Section';
+// styles //
+import { Container } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'id-1',
+        name: 'Rosie Simpson',
+        number: '459-12-56',
+        type: 'mobile',
+      },
+      {
+        id: 'id-2',
+        name: 'Hermione Kline',
+        number: '443-89-12',
+        type: 'work',
+      },
+      {
+        id: 'id-3',
+        name: 'Eden Clements',
+        number: '645-17-79',
+        type: 'mobile',
+      },
+      {
+        id: 'id-4',
+        name: 'Annie Copeland',
+        number: '227-91-26',
+        type: 'home',
+      },
+    ],
+    filter: '',
+  };
+
+  addContact = data => {
+    this.setState(prevState => ({
+      contacts: [data, ...prevState.contacts],
+    }));
+    Notify.success(`${data.name} has been added to contacts.`);
+  };
+
+  onFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  render() {
+    const contacts = this.getVisibleContacts();
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <ContactForm names={this.state} addContact={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter
+            filter={this.state.filter}
+            onFilterChange={this.onFilterChange}
+          />
+          <ContactList
+            contacts={contacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
